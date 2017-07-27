e776fe67101f9784b088bd84750f85e5c1732959
chienphamk58 <phamdinhchienk58@gmail.com>
2017-07-27 10:08:48 +0700

    chap12 password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: t("user.mailer.activation")
   end
 
-  def password_reset
-    @greeting = t "user.mailer.hi"
-    mail to: "to@example.org"
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("user.mailer.reset_pass")
   end
 end
